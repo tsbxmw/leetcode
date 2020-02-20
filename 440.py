c01c106f8a48f11cf00134ab990beaa92858e1ac
tsbxmw <1050636648@qq.com>
2020-02-20 14:55:57 +0800

    add new problem

--- 440.py
+++ 440.py
@@ -17,6 +17,9 @@
 # 链接：https://leetcode-cn.com/problems/k-th-smallest-in-lexicographical-order
 # 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 
+
+## 思路：
 class Solution:
     def findKthNumber(self, n: int, k: int) -> int:
+
         
