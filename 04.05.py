1dfc8518f8ec13353527440981a358d2e4ab4c17
mengwei <1050636648@qq.com>
2020-04-01 17:01:34 +0800

    add new solution to 04.05

--- 04.05.py
+++ 04.05.py
@@ -19,11 +19,13 @@
 #      根节点的值为 5 ，但是其右子节点值为 4 。
 
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 
 class Solution:
@@ -32,6 +34,8 @@ class Solution:
             return True
         self.temp = []
 
+        # 这里使用中序遍历为递增序列的原理
+
         def dfs(root):
             if root:
                 dfs(root.left)
@@ -39,3 +43,42 @@ class Solution:
                 dfs(root.right)
         dfs(root)
         return self.temp == sorted(set(self.temp))
+
+# 非递归方法
+# 迭代
+
+
+class Solution1:
+    def isValidBST(self, root: TreeNode) -> bool:
+        if not root:
+            return True
+
+        p = root
+        stack = []
+        rev = []
+        while p or stack:
+            while p:
+                stack.append(p)
+                p = p.left
+            if stack:
+                node = stack.pop()
+                rev.append(node.val)
+                p = node.right
+        return rev == sorted(set(rev))
+
+
+# 递归求解，不需要额外空间
+
+class Solution2:
+    def isValidBST(self, root: TreeNode) -> bool:
+        if not root:
+            return True
+
+        def dfs(node, min_=-float('inf'), max_=float('inf')):
+            if not node:
+                return True
+            if not (min_ < node.val < max_):
+                return False
+            return dfs(node.left, min_, node.val) and dfs(node.right, node.val, max_)
+
+        return dfs(root)
